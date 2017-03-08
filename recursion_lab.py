8e08b997d1d6eeddf761215942ef7405b261ad98
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-03-08 16:25:01 -0600

    commit2

--- recursion_lab.py
+++ recursion_lab.py
@@ -9,3 +9,40 @@ to change the appearance and behavior.
 Give your fractal a depth of at least 5.  Ensure the fractal is contained on the screen (at whatever size you set it).  Have fun.
 (35pts)
 '''
+
+import turtle
+import math
+
+my_turtle=turtle.Turtle()
+my_screen=turtle.Screen()
+color_list=["red","orange","yellow","green","blue","purple","magenta","cyan","black"]
+
+def draw(x,y,heading,dist,depth):
+    my_turtle.up()
+    my_turtle.goto(x,y)
+    my_turtle.color("black")
+    my_turtle.down()
+    my_turtle.setheading(heading)
+    my_turtle.forward(dist)
+
+    new_y=my_turtle.ycor()
+    new_x=my_turtle.xcor()
+    if depth>0:
+        for i in range(1,4):
+            draw(new_x,new_y,heading+45,dist/i,depth-1)
+            draw(new_x,new_y,heading-45,dist/i*(math.pi/3),depth-1)
+
+my_turtle.speed(10)
+for i in range(720):
+
+    my_turtle.setheading(math.pi+10*i)
+    my_turtle.forward(1+0.006*i)
+my_turtle.goto(-5,5)
+for i in range(1,1440,10):
+
+    my_turtle.setheading(math.pi*20*i)
+    my_turtle.forward(-i*1/2)
+for i in range(1,720,10):
+    draw(0,0,i,-i*1/2,1)
+
+my_screen.exitonclick()
--- recursion_problem_set.py
+++ recursion_problem_set.py
@@ -3,15 +3,59 @@
 Create a single recursive function (or more if you wish), which can answer the first three questions below.  For each question, make an appropriate call to the function. (5pts each)
 '''
 
-#1.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY, so MPR is APR/12).  Assuming you make no payments for 6 months, what is your new balance?  Solve recursively.
 
-#2.  You have $5000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  You make the minimum payment of $100 per month for 36 months.  What is your new balance?  Solve recursively.
+# 1.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY, so MPR is APR/12).  Assuming you make no payments for 6 months, what is your new balance?  Solve recursively.
+def solver(k, s):
+    while s >= 0:
+        if s == 0:
+            print(k)
+        k += k * (0.2 / 12)
+        s -= 1
+        return solver(k, s)
 
-#3.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  If you make the minimum payment of $100 per month, how many months will it take to pay it off?  Solve recursively.
+
+solver(10000, 6)
+
+
+# 2.  You have $5000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  You make the minimum payment of $100 per month for 36 months.  What is your new balance?  Solve recursively.
+def solver2(k, s):
+    while s >= 0:
+        if s == 0:
+            print(k)
+        k += k * (0.2 / 12)
+        k -= 100  # I assume you add the value after apr gets calculated
+        s -= 1
+        return solver2(k, s)
+
+
+solver2(10000, 36)
+
+
+# 3.  You have $10000 on a high interest credit card with an APR of 20.0% (calculated MONTHLY).  If you make the minimum payment of $100 per month, how many months will it take to pay it off?  Solve recursively.
+def solver3(k):
+    try:
+        while k > 0:
+            k += k * (0.2 / 12)
+            k -= 100  # I assume you add the value after apr gets calculated
+            return solver3(k)
+    except:
+        print("You will never repay your debt")
+
+
+solver3(10000)
 
 
 
 
 
+# 4  Pyramid of Cubes - (10pts) If you stack boxes in a pyramid, the top row would have 1 box, the second row would have two, the third row would have 3 and so on.  Make a recursive function which calculates the TOTAL NUMBER OF BOXES for a pyramid of boxes n high.  For instance, a pyramid that is 3 high would have a total of 6 boxes.  A pyramid 4 high would have 10.
+def pyramid(n,m):
+    if n>=0:
+        m += n
+        n -= 1
+        pyramid(n, m)
+        if n==0:
+            print(m)
 
-#4  Pyramid of Cubes - (10pts) If you stack boxes in a pyramid, the top row would have 1 box, the second row would have two, the third row would have 3 and so on.  Make a recursive function which calculates the TOTAL NUMBER OF BOXES for a pyramid of boxes n high.  For instance, a pyramid that is 3 high would have a total of 6 boxes.  A pyramid 4 high would have 10.
+
+pyramid(4,0)
